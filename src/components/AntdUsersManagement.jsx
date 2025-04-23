8ff11588e93205c726af40aa09693ffc1e5d17b0
AhrorSulaymonov <ahrorsulaymonov494@gmail.com>
2025-04-23 14:49:11 +0500

    first commit

--- src/components/AntdUsersManagement.jsx
+++ src/components/AntdUsersManagement.jsx
@@ -5,13 +5,17 @@ import React from "react";
 function AntdUsersManagement(props) {
   return (
     <div>
-      <Card hoverable className="form-wrapper">
+      <Card
+        hoverable
+        className="form-wrapper gap-3"
+        style={{ width: "600px", margin: "0 auto" }}
+      >
         <Title level={3}>New User</Title>
-        <Flex vertical style={{ width: "300px", margin: "0 auto" }}>
+        <Flex vertical gap={20}>
           <Input type="text" placeholder="Name" />
           <Input type="number" placeholder="Age" />
           <Input type="text" placeholder="Address" />
-          <Button>Save</Button>
+          <Button type="primary">Save</Button>
         </Flex>
       </Card>
     </div>
